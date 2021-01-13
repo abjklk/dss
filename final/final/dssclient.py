6c134fee2606f66acbb8d9629686e9d809b121e6
abjklk <abjklk@gmail.com>
2021-01-13 11:37:20 +0530

    13/01

--- final/final/dssclient.py
+++ final/final/dssclient.py
@@ -1,45 +1,35 @@
-import socket			 
-from linetimer import CodeTimer
-from Crypto.Hash import SHA256
-from Crypto.Signature import DSS
-from Crypto.PublicKey import DSA
-import time
-s = socket.socket()		 
+# Python program to check if a given password is strong, medium or weak. 
+def checkstrength(input_string): 
+	
+	n = len(input_string) 
 
-port = 10003				
+	# Checking lower alphabet in string 
+	hasLower = False
+	hasUpper = False
+	hasDigit = False
+	specialChar = False
+	normalChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+	
+	for i in range(n): 
+		if input_string[i].islower(): 
+			hasLower = True
+		if input_string[i].isupper(): 
+			hasUpper = True
+		if input_string[i].isdigit(): 
+			hasDigit = True
+		if input_string[i] not in normalChars: 
+			specialChar = True
 
-s.connect(('10.0.0.1', port)) 
+	# Strength of password 
+	print("Strength of password: ", end = "") 
+	if (hasLower and hasUpper and hasDigit and specialChar and n >= 8): 
+		print("Strong")
+	elif ((hasLower or hasUpper) and specialChar and n >= 6): 
+		print("Moderate")
+	else:
+		print("Weak") 
 
-x = s.recv(1084)
-msg1 = x[:1044]
-sig1 = x[1044:]
-print(msg1.decode())
-print(sig1)
+if __name__=="__main__": 
+	input_string = "GeeksforGeeks!@12"
+	checkstrength(input_string) 
 
-#Verify signature
-with open("dsskey.pem") as f:
-	publickey = DSA.import_key(f.read())
-
-def verify1(signature,content):	
-	pkey=DSS.new(publickey,'fips-186-3')
-	pkey.verify(content,signature)
-
-def hashit(content):
-	return SHA256.new(content)
-
-print(" DSS Hashing time")
-t0=time.time()
-digest1=hashit(msg1)
-t1=time.time()
-hashingtime=(t1-t0)*1000
-
-print("\n\n DSS verification time")
-t0=time.time()
-verify1(sig1,digest1)
-t1=time.time()
-verificationtime=(t1-t0)*1000
-
-with open("results.txt","a") as f:
-	f.write("DSS Hashing time " +str(hashingtime)+"\n"+ "DSS verification time " + str(verificationtime)+"\n")
-
-s.close()	 
